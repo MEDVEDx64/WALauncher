dee7616f299d588dddba864958910967052f21f8
Andrey Bobkov <medvedx64@gmail.com>
2016-09-07 10:01:55 +0300

    Disabling install button after click

--- WALauncher/ViewModels/WapkgObjects/AvailablePackage.cs
+++ WALauncher/ViewModels/WapkgObjects/AvailablePackage.cs
@@ -8,16 +8,20 @@ namespace WALauncher.ViewModels.WapkgObjects
         string targetDistro;
 
         public DelegateCommand InstallCommand { get; }
+        public bool ButtonEnabled { get; private set; }
 
         public AvailablePackage(string targetDistro)
         {
             this.targetDistro = targetDistro;
             InstallCommand = new DelegateCommand(Install);
+            ButtonEnabled = true;
         }
 
         void Install()
         {
             InteractionService.Get().InstallPackage(targetDistro, Name);
+            ButtonEnabled = false;
+            RaisePropertyChanged(nameof(ButtonEnabled));
         }
     }
 }
