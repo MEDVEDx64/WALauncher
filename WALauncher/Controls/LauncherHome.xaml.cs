4b7ba0fcc60602ed80a308fa2b8ee64180ee9475
Andrey Bobkov <medvedx64@gmail.com>
2016-09-18 18:48:05 +0300

    Some visual fixes / added homepage link

--- WALauncher/Controls/LauncherHome.xaml.cs
+++ WALauncher/Controls/LauncherHome.xaml.cs
@@ -1,28 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Diagnostics;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WALauncher.Controls
 {
-    /// <summary>
-    /// Interaction logic for LauncherHome.xaml
-    /// </summary>
     public partial class LauncherHome : UserControl
     {
         public LauncherHome()
         {
             InitializeComponent();
+        }
+
+        private void OnHomepageLinkRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(e.Uri.ToString());
         }
     }
 }
