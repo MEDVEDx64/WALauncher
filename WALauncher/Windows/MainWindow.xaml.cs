1631b0e9e060c7022f01a442441a3d33006c9353
Andrey Bobkov <medvedx64@gmail.com>
2017-02-15 15:55:12 +0300

    Vertical window resizing

--- WALauncher/Windows/MainWindow.xaml.cs
+++ WALauncher/Windows/MainWindow.xaml.cs
@@ -1,20 +1,36 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Interop;
 using WALauncher.Wapkg;
 
 namespace WALauncher.Windows
 {
     public partial class MainWindow : Window
     {
+        [DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        const int GWL_STYLE = -16;
+        const int WS_MAXIMIZEBOX = 0x10000;
+
         public MainWindow()
         {
             InitializeComponent();
             Loaded += OnLoaded;
             Closed += OnClosed;
+            StateChanged += OnWindowStateChanged;
         }
 
         void OnLoaded(object sender, EventArgs e)
         {
+            // Disabling maximize button
+            var hwnd = new WindowInteropHelper(this).Handle;
+            SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_MAXIMIZEBOX);
+
             var wapkg = InteractionService.Get();
 
             wapkg.Subscribe();
@@ -27,6 +43,14 @@ namespace WALauncher.Windows
         void OnClosed(object sender, EventArgs e)
         {
             InteractionService.Get().Shutdown();
+        }
+
+        private void OnWindowStateChanged(object sender, EventArgs e)
+        {
+            if(WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+            }
         }
     }
 }
