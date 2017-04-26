7569d065f9471e59dc220df2d0511d0ad807d887
Andrey Bobkov <medvedx64@gmail.com>
2017-04-26 17:34:34 +0300

    Localization, sir!

--- WALauncher/Properties/Resources.Designer.cs
+++ WALauncher/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace WALauncher.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace WALauncher.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WALauncher.Properties.Resources", typeof(Resources).Assembly);
@@ -51,12 +51,210 @@ namespace WALauncher.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Add.
+        /// </summary>
+        public static string BtnAdd {
+            get {
+                return ResourceManager.GetString("BtnAdd", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Install.
+        /// </summary>
+        public static string BtnInstall {
+            get {
+                return ResourceManager.GetString("BtnInstall", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Move down.
+        /// </summary>
+        public static string BtnMoveDown {
+            get {
+                return ResourceManager.GetString("BtnMoveDown", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Move up.
+        /// </summary>
+        public static string BtnMoveUp {
+            get {
+                return ResourceManager.GetString("BtnMoveUp", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Remove.
+        /// </summary>
+        public static string BtnRemove {
+            get {
+                return ResourceManager.GetString("BtnRemove", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Distro.
+        /// </summary>
+        public static string TabDistro {
+            get {
+                return ResourceManager.GetString("TabDistro", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Home.
+        /// </summary>
+        public static string TabHome {
+            get {
+                return ResourceManager.GetString("TabHome", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Packages.
+        /// </summary>
+        public static string TabPackages {
+            get {
+                return ResourceManager.GetString("TabPackages", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Sources.
+        /// </summary>
+        public static string TabSources {
+            get {
+                return ResourceManager.GetString("TabSources", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Add package....
+        /// </summary>
+        public static string TbAddPackage {
+            get {
+                return ResourceManager.GetString("TbAddPackage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to WALauncher has crashed..
+        /// </summary>
+        public static string TbCrashed {
+            get {
+                return ResourceManager.GetString("TbCrashed", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Install from file.
+        /// </summary>
+        public static string TbInstallFromFile {
+            get {
+                return ResourceManager.GetString("TbInstallFromFile", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to No distributions installed..
+        /// </summary>
+        public static string TbNoDistsInstalled {
+            get {
+                return ResourceManager.GetString("TbNoDistsInstalled", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to No sources were added..
+        /// </summary>
+        public static string TbNoSources {
+            get {
+                return ResourceManager.GetString("TbNoSources", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to package from file....
+        /// </summary>
+        public static string TbPackageFromFile {
+            get {
+                return ResourceManager.GetString("TbPackageFromFile", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Suggested name:.
+        /// </summary>
+        public static string TbSuggestedName {
+            get {
+                return ResourceManager.GetString("TbSuggestedName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Try adding one.
+        /// </summary>
+        public static string TbTryAddingOne {
+            get {
+                return ResourceManager.GetString("TbTryAddingOne", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Visit homepage for the latest version.
+        /// </summary>
+        public static string TbVisitHomepage {
+            get {
+                return ResourceManager.GetString("TbVisitHomepage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Launch.
+        /// </summary>
+        public static string TipLaunch {
+            get {
+                return ResourceManager.GetString("TipLaunch", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Open in File Explorer.
+        /// </summary>
+        public static string TipOpenExplorer {
+            get {
+                return ResourceManager.GetString("TipOpenExplorer", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Refresh.
+        /// </summary>
+        public static string TipRefresh {
+            get {
+                return ResourceManager.GetString("TipRefresh", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Error.
+        /// </summary>
+        public static string TitleError {
+            get {
+                return ResourceManager.GetString("TitleError", resourceCulture);
             }
         }
     }
--- WALauncher/ViewModels/WapkgObjects/AvailableDistribution.cs
+++ WALauncher/ViewModels/WapkgObjects/AvailableDistribution.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using WALauncher.Properties;
 using WALauncher.Utils;
 using WALauncher.Wapkg;
 
@@ -32,7 +33,7 @@ namespace WALauncher.ViewModels.WapkgObjects
 
                 else
                 {
-                    return "Install";
+                    return Resources.BtnInstall;
                 }
             }
         }
